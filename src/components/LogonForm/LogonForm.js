a39c4e710973aa63e9e2cebebf473496e8b4f4e9
jellyruby <50604410+jellyruby@users.noreply.github.com>
2025-07-22 12:16:45 +0900

    feat: userCreate
    계정 생성UI 제작

--- src/components/LogonForm/LogonForm.js
+++ src/components/LogonForm/LogonForm.js
@@ -1,41 +1,43 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Button from '@components/Button';
-import styles from './LoginForm.module.scss';
+import styles from './LogonForm.module.scss';
 
-const LoginForm = () => {
+const LogonForm = () => {
   const [userid, setUserid] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [locate, setLocate] = useState('');
   const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-            const response = await fetch('http://localhost:8888/auth/login', {
+            const response = await fetch('http://localhost:8888/users', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ userid, password }),
+        body: JSON.stringify({ userid, password, name, locate }),
         credentials: 'include', // 쿠키를 주고받기 위해 추가
       });
 
       if (response.ok) {
         const data = await response.json();
-        console.log('Login successful:', data);
+        console.log('Logon successful:', data);
         router.push('/');
       } else {
         alert('로그인에 실패했습니다.');
       }
     } catch (error) {
-      console.error('Login failed:', error);
+      console.error('Logon failed:', error);
       alert('로그인 중 오류가 발생했습니다.');
     }
   };
 
   return (
     <div>
-      <p className={styles.head}>로그인</p>
+      <p className={styles.head}>계정 생성</p>
       <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.formGroup}>
           <label htmlFor="userid">아이디</label>
@@ -55,13 +57,28 @@ const LoginForm = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <div>
-          <Button type="submit" className={styles.LoginFormBtn}>로그인</Button>
-          <Button type="button" onClick={() => router.push('/logon/')} className={styles.LoginFormBtn} >계정 생성</Button>
+        <div className={styles.formGroup}>
+          <label htmlFor="password">이름</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="locate">주소</label>
+          <input
+            type="text"
+            id="locate"
+            value={locate}
+            onChange={(e) => setLocate(e.target.value)}
+          />
+        </div>
+        <Button type="submit">생성</Button>
       </form>
     </div>
   );
 };
 
-export default LoginForm;
+export default LogonForm;
---
+++ src/pages/logon.js
@@ -0,0 +1,56 @@
+import Layout from '@components/Layout';
+import LogonForm from '@components/LogonForm';
+
+const LogonPage = () => {
+  return (
+    <Layout center>
+      <LogonForm/>
+    </Layout>
+  );
+};
+
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const { cookie } = req.headers;
+
+  // 쿠키가 없는 경우 종료
+  if (!cookie) {
+    return {
+      props: {
+      },
+    };
+  }
+
+  try {
+    const response = await fetch('http://localhost:8888/auth/profile', {
+      headers: {
+        cookie,
+      },
+    });
+
+    // 인증 성공 시 로그인 페이지로 리디렉션
+    if (response.ok) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+    }
+
+    const user = await response.json();
+
+    // 인증 성공 시 user 정보를 props로 전달
+    return {
+      props: {
+        user,
+      },
+    };
+  } catch (error) {
+    console.error('Failed to fetch auth profile:', error);
+  }
+}
+
+
+export default LogonPage;
